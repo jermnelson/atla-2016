ef234712884ba1dd0831756a43d1b9d45aae82d7
Jeremy Nelson <jermnelson@gmail.com>
2016-03-02 11:04:17 -0700

    initial commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+__author__ = "Jeremy Nelson"
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+@app.route("/")
+def page(name=None):
+    if name is None:
+        template_name = "index.html"
+    else:
+        template_name = "{}.html".format(name)
+    return render_template(template_name)
+
+if __name__ == '__main__':
+    app.run()
